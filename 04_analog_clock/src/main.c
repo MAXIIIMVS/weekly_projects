60e9cb8518fe2789f492acfb6775e5d6e1dc70d1
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2026-02-16 21:11:38 +0330

    04_analog_clock: draw minute markers.

--- 04_analog_clock/src/main.c
+++ 04_analog_clock/src/main.c
@@ -1,6 +1,31 @@
 #include "raylib.h"
-#include <stdio.h>
-#include <time.h>
+#include <math.h>
+
+float deg_to_rad(float deg) { return deg * (PI / 180.0f); }
+
+void draw_minute_marks(const Vector2 *const circle_center, float radius,
+                       float start_angle_rad) {
+  const float big_length = 6.0f;
+  const float small_length = 4.0f;
+  const float step = 2.0f * PI / 60.0f; // 6°
+
+  for (int i = 0; i < 60; ++i) {
+    float angle = start_angle_rad + i * step;
+
+    float marker_length = (i % 5 == 0) ? big_length : small_length;
+    float inner = radius - marker_length;
+
+    float c = cosf(angle);
+    float s = sinf(angle);
+
+    float x1 = circle_center->x + inner * c;
+    float y1 = circle_center->y + inner * s;
+    float x2 = circle_center->x + radius * c;
+    float y2 = circle_center->y + radius * s;
+
+    DrawLineEx((Vector2){x1, y1}, (Vector2){x2, y2}, marker_length, DARKGRAY);
+  }
+}
 
 typedef struct {
   const int width;
@@ -10,8 +35,10 @@ typedef struct {
 
 int main(void) {
   WindowInfo window = {.width = 300, .height = 300, .title = "Clock"};
+  Vector2 circle_center = {.x = window.width / 2.0f, .y = window.height / 2.0f};
+  float circle_radius = (window.height / 2.0f) - 20.0f;
 
-  const Color BG = {18, 32, 47, 255};      // deep navy
+  const Color BG = BEIGE;
   const Color FACE = {245, 240, 220, 255}; // warm cream
 
   // time_t now = time(NULL);
@@ -23,8 +50,8 @@ int main(void) {
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BG);
-    DrawCircle(window.width / 2, window.height / 2,
-               (window.height / 2.0f) - 20.0f, FACE);
+    DrawCircle(circle_center.x, circle_center.y, circle_radius, FACE);
+    draw_minute_marks(&circle_center, circle_radius, deg_to_rad(-90));
     EndDrawing();
   }
 
