f47dbe8a33b51e681d6ff432be5f399de5add65d
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2026-02-16 19:31:35 +0330

    04_analog_clock: draw a circle on screen.

--- 04_analog_clock/src/main.c
+++ 04_analog_clock/src/main.c
@@ -1,7 +1,33 @@
-#include <raylib.h>
+#include "raylib.h"
 #include <stdio.h>
+#include <time.h>
+
+typedef struct {
+  const int width;
+  const int height;
+  const char *title;
+} WindowInfo;
 
 int main(void) {
-  printf("hi");
+  WindowInfo window = {.width = 300, .height = 300, .title = "Clock"};
+
+  const Color BG = {18, 32, 47, 255};      // deep navy
+  const Color FACE = {245, 240, 220, 255}; // warm cream
+
+  // time_t now = time(NULL);
+  // struct tm *t = localtime(&now);
+
+  InitWindow(window.width, window.height, window.title);
+  SetTargetFPS(60); // this is a clock, more than 60 is redundant
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BG);
+    DrawCircle(window.width / 2, window.height / 2,
+               (window.height / 2.0f) - 20.0f, FACE);
+    EndDrawing();
+  }
+
+  CloseWindow();
   return 0;
 }
